f0f7d8d41fcd53c73b36ee7c310af7753c2ae205
paulief <paulfagan1991@gmail.com>
2016-04-05 14:12:44 -0700

    Recursive palindrome check with regex

---
+++ palindrome_recursive.js
@@ -0,0 +1,12 @@
+function palindrome(str) {
+  // Good luck!
+  var noPunc = str.replace(/[\W+_]/g, '').toLowerCase().split('');
+  return palRecursive(noPunc, 0);
+}
+
+function palRecursive(chars, idx) {
+  if (idx > chars.length / 2 - 1) return true;
+  if (chars[idx] !== chars[chars.length - idx - 1]) return false;
+  
+  return pal(chars, idx + 1);
+}

be8099987d47bcea8f777009fb3fc032d816c9a0
paulief <paulfagan1991@gmail.com>
2016-04-05 13:42:15 -0700

    Clean up spacing

--- fibonacci_simple.rb
+++ fibonacci_simple.rb
@@ -1,10 +1,10 @@
 # https://www.hackerrank.com/contests/programming-interview-questions/challenges/fibonacci-lite
 
 def fib(n)
-    return 1 if n == 1
-    return 0 if n == 0
+  return 1 if n == 1
+  return 0 if n == 0
     
-    return fib(n-1) + fib(n-2)
+  return fib(n-1) + fib(n-2)
 end
 
 inp = Integer($stdin.read)
